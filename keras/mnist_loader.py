e1b70767f20ae00c70e5f73b52782056162735b0
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-03-28 20:28:15 +0200

    concat validation data to the training data

--- keras/mnist_loader.py
+++ keras/mnist_loader.py
@@ -4,5 +4,10 @@ import gzip
 def load_data():
     f = gzip.open('./mnist.pkl.gz', 'rb')
     training_data, validation_data, test_data = pickle.load(f, encoding='latin1')
+
+    images = np.concatenate((training_set[0], validation_set[0]))
+    lables = np.concatenate((training_set[1], validation_set[1]))
+    training_set = (images, lables)
+
     f.close()
     return (training_data, validation_data, test_data)
