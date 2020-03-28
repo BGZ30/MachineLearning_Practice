e7d6d5abc6cc47a6e2328c780ee783bc839653e9
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-03-28 23:29:48 +0200

    fixed the loading steps

--- keras/mnist_loader.py
+++ keras/mnist_loader.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pickle
 import gzip
 
@@ -5,9 +6,10 @@ def load_data():
     f = gzip.open('./mnist.pkl.gz', 'rb')
     training_data, validation_data, test_data = pickle.load(f, encoding='latin1')
 
-    images = np.concatenate((training_set[0], validation_set[0]))
-    lables = np.concatenate((training_set[1], validation_set[1]))
-    training_set = (images, lables)
+    images = np.concatenate((training_data[0], validation_data[0]))
+    lables = np.concatenate((training_data[1], validation_data[1]))
+    training_data = (images, lables)
 
     f.close()
-    return (training_data, validation_data, test_data)
+    # return (training_data, validation_data, test_data)
+    return (training_data, test_data)
