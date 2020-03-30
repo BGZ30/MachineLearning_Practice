c7a50062725808a41caf1913094d63666b7db65e
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-03-30 15:31:02 +0200

    added the pooling effect

--- CNN/mnist_CNN.py
+++ CNN/mnist_CNN.py
@@ -1,10 +1,14 @@
 import mnist
 from convLayer import Conv3x3
+from maxPooling import MaxPool2
 
 training_set = mnist.training_images()
 training_lables = mnist.training_lables()
 
 conv = Conv3x3(8)  # conv layer with 8 filters
+pool = MaxPool2()
+
 output = conv.forward(training_set[0])  # test for the first image
+output = pool.forward(output)  # pass to the pooling layer
 
 print(output.shape)
